9045ff36f53268645245656119a0aa1057bc18ab
Luke Lanchester <luke@lukelanchester.com>
2015-06-28 11:23:12 +0100

    Base server

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').load();
+
+
+var express = require('express');
+var app = express();
+app.disable('etag');
+app.disable('x-powered-by');
+
+
+app.use('/' + process.env.WEB_PATH, express.static('public'));
+
+
+var server = app.listen(process.env.WEB_PORT || 3001, function(){
+
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('Elbrus listening at http://%s:%s', host, port);
+
+});
